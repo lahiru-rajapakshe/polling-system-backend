46821d567634bbf6160fbd44252c28ab8c6c27c2
LahiruRJ <sanlahiru720@gmail.com>
2022-06-10 12:30:09 +0530

    Test classes

--- src/main/java/lk/lahiru/pollbackend/service/util/JPAUtil.java
+++ src/main/java/lk/lahiru/pollbackend/service/util/JPAUtil.java
@@ -2,14 +2,26 @@ package lk.lahiru.pollbackend.service.util;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.IOException;
+import java.util.Properties;
 
-public class JPAUtil {
-    private static EntityManagerFactory emf= buildEntityManagerFactory();
+public abstract class JPAUtil {
+
+    private static EntityManagerFactory emf = buildEntityManagerFactory();
 
     private static EntityManagerFactory buildEntityManagerFactory() {
-        return Persistence.createEntityManagerFactory("poll");
+        Properties prop = new Properties();
+        try {
+            prop.load(JPAUtil.class.getResourceAsStream("/application-test.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Persistence.createEntityManagerFactory("poll", prop);
     }
-    public static  EntityManagerFactory entityManagerFactory(){
+
+    public static EntityManagerFactory getEntityManagerFactory(){
         return emf;
     }
+
 }
+
---
+++ src/test/java/lk/lahiru/pollbackend/service/util/JPAUtilTest.java
@@ -0,0 +1,31 @@
+package lk.lahiru.pollbackend.service.util;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JPAUtilTest {
+
+    @Test
+    void getEntityManagerFactory() {
+        // given
+        EntityManagerFactory instance1 = JPAUtil.getEntityManagerFactory();
+        // when
+        EntityManagerFactory instance2 = JPAUtil.getEntityManagerFactory();
+        // then
+        assertEquals(instance1, instance2);
+    }
+
+    @Test
+    void getEntityManager() {
+        // given
+        EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
+        // when
+        EntityManager em = emf.createEntityManager();
+        // then
+        assertNotNull(em);
+    }
+}
