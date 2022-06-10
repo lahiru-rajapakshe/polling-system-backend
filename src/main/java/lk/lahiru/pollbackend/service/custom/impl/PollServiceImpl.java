77392ff7fc8c68abb6c9fd26d7d11d0d2381bb9a
LahiruRJ <sanlahiru720@gmail.com>
2022-06-10 17:47:09 +0530

    Implement POllServiceImpl

--- src/main/java/lk/lahiru/pollbackend/service/custom/impl/PollServiceImpl.java
+++ src/main/java/lk/lahiru/pollbackend/service/custom/impl/PollServiceImpl.java
@@ -1,26 +1,42 @@
 package lk.lahiru.pollbackend.service.custom.impl;
 
-
-import lk.lahiru.pollbackend.dto.PollDTO;
-import lk.lahiru.pollbackend.service.custom.PollService;
-import lk.lahiru.pollbackend.service.util.JPAUtil;
+import lk.ijse.dep8.polling.dao.DAOFactory;
+import lk.ijse.dep8.polling.dao.custom.PollDAO;
+import lk.ijse.dep8.polling.dto.PollDTO;
+import lk.ijse.dep8.polling.service.custom.PollService;
+import lk.ijse.dep8.polling.service.exception.NotFoundException;
+import lk.ijse.dep8.polling.service.util.EntityDTOTransformer;
+import lk.ijse.dep8.polling.service.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PollServiceImpl implements PollService {
 
     @Override
     public List<PollDTO> listAllPolls() {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
-
-        em.close();
-        return null;
+        PollDAO pollDAO = DAOFactory.getInstance().getDAO(em, DAOFactory.DAOType.POLL);
+        try {
+            return pollDAO.findAll().stream().map(EntityDTOTransformer::getPollDTO)
+                    .collect(Collectors.toList());
+        }finally{
+            em.close();
+        }
     }
 
     @Override
-    public PollDTO getPoll(int id) {
-        return null;
+    public PollDTO getPoll(int id) throws NotFoundException {
+        EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
+        PollDAO pollDAO = DAOFactory.getInstance().getDAO(em, DAOFactory.DAOType.POLL);
+        try {
+            return pollDAO.findById(id)
+                    .map(EntityDTOTransformer::getPollDTO)
+                    .orElseThrow(() -> new NotFoundException("Invalid Poll ID"));
+        }finally{
+            em.close();
+        }
     }
 
     @Override
