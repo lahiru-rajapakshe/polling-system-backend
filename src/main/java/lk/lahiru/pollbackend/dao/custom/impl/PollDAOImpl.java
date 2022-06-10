58ca7d80ab86d088ed6600ded5de901e4a854cd8
LahiruRJ <sanlahiru720@gmail.com>
2022-06-10 19:41:40 +0530

    Fix PollDAOImpl issue

--- src/main/java/lk/lahiru/pollbackend/dao/custom/impl/PollDAOImpl.java
+++ src/main/java/lk/lahiru/pollbackend/dao/custom/impl/PollDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.lahiru.pollbackend.dao.custom.impl;
 
+
 import lk.lahiru.pollbackend.dao.CrudDAOImpl;
 import lk.lahiru.pollbackend.dao.custom.PollDAO;
 import lk.lahiru.pollbackend.entity.Poll;
@@ -7,8 +8,8 @@ import lk.lahiru.pollbackend.entity.Poll;
 import javax.persistence.EntityManager;
 
 public class PollDAOImpl extends CrudDAOImpl<Poll, Integer> implements PollDAO {
+
     public PollDAOImpl(EntityManager em) {
         this.em = em;
     }
-
 }
