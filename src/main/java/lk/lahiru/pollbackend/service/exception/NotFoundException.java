6dce59c97c1545734691ead20cd7b78a64ea5eee
LahiruRJ <sanlahiru720@gmail.com>
2022-06-10 18:05:37 +0530

    Add notFoundException class

---
+++ src/main/java/lk/lahiru/pollbackend/service/exception/NotFoundException.java
@@ -0,0 +1,12 @@
+package lk.lahiru.pollbackend.service.exception;
+
+public class NotFoundException extends Exception{
+
+    public NotFoundException() {
+        super();
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
