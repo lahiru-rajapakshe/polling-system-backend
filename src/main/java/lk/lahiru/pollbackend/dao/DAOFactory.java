8bbe63983412dd1de2c0637a33229dd19282a62e
LahiruRJ <sanlahiru720@gmail.com>
2022-06-10 19:39:10 +0530

    Add daoFactory

--- src/main/java/lk/lahiru/pollbackend/dao/DAOFactory.java
+++ src/main/java/lk/lahiru/pollbackend/dao/DAOFactory.java
@@ -1,4 +1,31 @@
 package lk.lahiru.pollbackend.dao;
 
+import lk.ijse.dep8.polling.dao.custom.impl.PollDAOImpl;
+
+import javax.persistence.EntityManager;
+
 public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+
+    }
+
+    public static DAOFactory getInstance(){
+        return (daoFactory == null) ? (daoFactory = new DAOFactory()) : daoFactory;
+    }
+
+    public <T extends  SuperDAO> T getDAO(EntityManager em, DAOType daoType){
+        switch (daoType) {
+            case POLL:
+                return (T) new PollDAOImpl(em);
+            default:
+                return null;
+        }
+    }
+
+    public enum DAOType{
+        POLL
+    }
 }
